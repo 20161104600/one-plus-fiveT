274bbb1cb93c50f640df32c630a7921377331b11
20161104600 <guohua@ciec-imacs-imac.local>
2017-12-14 10:34:50 +0800

    Initial Commit

---
+++ one plus fiveT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  one plus fiveT
+//
+//  Created by 国华 on 17/12/14.
+//  Copyright © 2017年 国华. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
